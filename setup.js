5527639f87f988573b0cd77590fb350b94dea739
Ryan Cheng <rcheng07@stanford.edu>
2023-01-15 01:40:25 +0100

    basic networking setup

--- setup.js
+++ setup.js
@@ -25,18 +25,123 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.listen = exports.connect = void 0;
 const net = __importStar(require("net"));
+const json_canonicalize_1 = require("json-canonicalize");
 const port = 18018;
+var MESSAGE_TYPES;
+(function (MESSAGE_TYPES) {
+    MESSAGE_TYPES[MESSAGE_TYPES["HELLO"] = 0] = "HELLO";
+    MESSAGE_TYPES[MESSAGE_TYPES["PEERS_REQUEST"] = 1] = "PEERS_REQUEST";
+    MESSAGE_TYPES[MESSAGE_TYPES["CHAINTIP"] = 2] = "CHAINTIP";
+    MESSAGE_TYPES[MESSAGE_TYPES["MEMPOOL"] = 3] = "MEMPOOL";
+    MESSAGE_TYPES[MESSAGE_TYPES["GET_OBJECT"] = 4] = "GET_OBJECT";
+    MESSAGE_TYPES[MESSAGE_TYPES["INVALID"] = 5] = "INVALID";
+})(MESSAGE_TYPES || (MESSAGE_TYPES = {}));
+function parse_message(received_message) {
+    let result = [];
+    let messages = received_message.split("\n");
+    for (let i = 0; i < messages.length; i++) {
+        try {
+            if (messages[i] == '') {
+                continue;
+            }
+            let parsed_json = JSON.parse(messages[i]);
+            switch (parsed_json["type"]) {
+                case "hello":
+                    result.push(MESSAGE_TYPES.HELLO);
+                    break;
+                case "getpeers":
+                    result.push(MESSAGE_TYPES.PEERS_REQUEST);
+                    break;
+                case "getchaintip":
+                    result.push(MESSAGE_TYPES.CHAINTIP);
+                    break;
+                case "getmempool":
+                    result.push(MESSAGE_TYPES.MEMPOOL);
+                    break;
+                case "getobject":
+                    result.push(MESSAGE_TYPES.GET_OBJECT);
+                    break;
+                default:
+                    result.push(MESSAGE_TYPES.INVALID);
+            }
+        }
+        catch (err) {
+            result[i] = MESSAGE_TYPES.INVALID;
+        }
+    }
+    if (result[messages.length - 1] == MESSAGE_TYPES.INVALID) {
+        return [messages[messages.length - 1], result];
+    }
+    return ['', result];
+}
+function prepare_message(message_type) {
+    let json_message;
+    switch (message_type) {
+        case MESSAGE_TYPES.HELLO:
+            json_message = {
+                type: "hello",
+                version: "0.9.0",
+                agent: "Marabu-Core Client 0.9"
+            };
+            break;
+        case MESSAGE_TYPES.PEERS_REQUEST:
+            json_message = {
+                "type": "peers",
+                "peers": [],
+            };
+            break;
+        case MESSAGE_TYPES.MEMPOOL:
+            json_message = {
+                "type": "mempool",
+                "txids": [],
+            };
+            break;
+        case MESSAGE_TYPES.CHAINTIP:
+            json_message = {
+                "type": "chaintip",
+                "blockid": "0024839ec9632d382486ba7aac7e0bda3b4bda1d4bd79be9ae78e7e1e813ddd8"
+            };
+            break;
+        case MESSAGE_TYPES.GET_OBJECT:
+            break;
+        case MESSAGE_TYPES.INVALID:
+            json_message = {
+                "type": "error",
+                "name": "INVALID_FORMAT",
+                "description": "invalid format received"
+            };
+            break;
+    }
+    if (!json_message) {
+        return "";
+    }
+    let canonicalized_output = (0, json_canonicalize_1.canonicalize)(json_message);
+    return canonicalized_output;
+}
 function connect() {
     const client = new net.Socket();
+    let handshake_completed = false;
+    let buffer = '';
     client.connect(port, '45.63.84.226', () => {
         console.log('Connected');
     });
     client.on('data', (data) => {
+        let messages;
+        [buffer, messages] = parse_message(buffer + data);
         console.log('Received: ' + data);
+        for (const message of messages) {
+            let response = prepare_message(message);
+            if (response != "") {
+                console.log(response);
+                client.write(response + '\n');
+            }
+        }
+        client.write(`{"type":"getpeers"}\n`);
+        // let response: string = prepare_message(MESSAGE_TYPES.REQUEST_PEERS);
+        // client.write(response + "\n");
     });
     client.on('error', (err) => {
         console.error('Error: ' + err);
-        client.destroy();
     });
     client.on('close', () => {
         console.log('Connection closed');
--- setup.ts
+++ setup.ts
@@ -1,21 +1,131 @@
 import * as net from 'net';
+import { canonicalize } from 'json-canonicalize';
 
 const port: number = 18018;
 
+enum MESSAGE_TYPES {
+    HELLO,
+    PEERS_REQUEST,
+    CHAINTIP,
+    MEMPOOL,
+    GET_OBJECT,
+    INVALID
+}
+
+function parse_message(received_message: string): [string, Array<MESSAGE_TYPES>] {
+
+    let result: Array<MESSAGE_TYPES> = [];
+
+    let messages: Array<string> = received_message.split("\n");
+    for (let i = 0; i < messages.length; i++) {
+        try {
+            if (messages[i] == '') {
+                continue;
+            }
+            let parsed_json = JSON.parse(messages[i]);
+            switch (parsed_json["type"]) {
+                case "hello":
+                    result.push(MESSAGE_TYPES.HELLO);
+                    break;
+                case "getpeers":
+                    result.push(MESSAGE_TYPES.PEERS_REQUEST);
+                    break;
+                case "getchaintip":
+                    result.push(MESSAGE_TYPES.CHAINTIP);
+                    break;
+                case "getmempool":
+                    result.push(MESSAGE_TYPES.MEMPOOL);
+                    break;
+                case "getobject":
+                    result.push(MESSAGE_TYPES.GET_OBJECT);
+                    break;
+                default:
+                    result.push(MESSAGE_TYPES.INVALID);
+            }
+        } catch (err) {
+            result[i] = MESSAGE_TYPES.INVALID;
+        }
+    }
+
+    if (result[messages.length - 1] == MESSAGE_TYPES.INVALID) {
+        return [messages[messages.length - 1], result];
+    }
+
+    return ['', result];
+
+}
+
+function prepare_message(message_type: MESSAGE_TYPES): string {
+    let json_message;
+    switch (message_type) {
+        case MESSAGE_TYPES.HELLO:
+            json_message = {
+                type: "hello",
+                version: "0.9.0",
+                agent: "Marabu-Core Client 0.9"
+            }
+            break;
+        case MESSAGE_TYPES.PEERS_REQUEST:
+            json_message = {
+                "type": "peers",
+                "peers": [],
+            }
+            break;
+        case MESSAGE_TYPES.MEMPOOL:
+            json_message = {
+                "type": "mempool",
+                "txids": [],
+            }
+            break;
+        case MESSAGE_TYPES.CHAINTIP:
+            json_message = {
+                "type": "chaintip",
+                "blockid": "0024839ec9632d382486ba7aac7e0bda3b4bda1d4bd79be9ae78e7e1e813ddd8"
+            }
+            break;
+        case MESSAGE_TYPES.GET_OBJECT:
+            break;
+        case MESSAGE_TYPES.INVALID:
+            json_message = {
+                "type": "error",
+                "name": "INVALID_FORMAT",
+                "description": "invalid format received"
+            }
+            break;
+
+    }
+    if (!json_message) {
+        return "";
+    }
+    let canonicalized_output: string = canonicalize(json_message);
+    return canonicalized_output;
+}
+
 export function connect() {
     const client = new net.Socket();
+    let handshake_completed = false;
+
+    let buffer: string = '';
 
     client.connect(port, '45.63.84.226', () => {
         console.log('Connected');
     });
 
     client.on('data', (data: string) => {
+        let messages: Array<MESSAGE_TYPES>;
+        [buffer, messages] = parse_message(buffer + data);
         console.log('Received: ' + data);
+        for (const message of messages) {
+            let response: string = prepare_message(message);
+            if (response != "") {
+                console.log(response);
+                client.write(response + '\n');
+            }
+        }
     });
 
     client.on('error', (err: string) => {
         console.error('Error: ' + err);
-        client.destroy();
     });
 
     client.on('close', () => {
