e552ff62630a02d8c668d3e770d50ca2adeec757
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-01-18 03:33:43 -0800

    additional tests, not entirely working

--- tests/networking_tests.ts
+++ tests/networking_tests.ts
@@ -4,6 +4,60 @@ function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const port: number = 18018;
+const ip_address: string = "127.0.0.1";
+
+// invalid connection error
+async function test_connection() {
+    const test_node = new LightNode();
+    test_node._client.connect(port, ip_address, () => {});
+    await sleep(1000);
+
+    if (test_node._client.connecting == false) {
+        let json_message: any = {
+            "type": "Connected to Node",
+        };
+        test_node._write(json_message);
+    }
+}
+
+async function test_hello() {
+    const test_node = new LightNode();
+    // FILL THIS IN
+}
+
+async function test_reconnection() {
+    const test_node = new LightNode();
+    test_node._client.destroy();
+    test_node._client.connect(port, ip_address, () => {});
+
+    if (test_node._client.connecting == false) {
+        let json_message: any = {
+            "type": "Connected to Node",
+        };
+        test_node._write(json_message);
+    }
+}
+
+function test_segmented_message() {
+    const test_node = new LightNode();
+    test_node._client.connect(port, ip_address, () => {});
+    test_node._client.write("{\"type\":\"ge\"}");
+    setTimeout(() => {
+        test_node._client.write(", \"tpeers\"}");
+    }, 100);
+
+    test_node._client.on('data', (data: string) => {
+        const received_message = JSON.parse(data.toString());
+        if (received_message.type == "getpeers" && Array.isArray(received_message.peers)) {
+            let json_message: any = {
+                "type": "Sucess on Segmented Message"
+            };
+            test_node._write(json_message);
+        }
+    });
+}
+
 // invalid handshake error
 async function test_handshake() {
 
@@ -56,13 +110,25 @@ async function test_buffer_timeout() {
 }
 
 async function run_tests() {
-    await test_handshake();
+
+    await test_connection();
     await sleep(1000);
     console.log("\n---------------------------- \n");
-    await test_buffer();
-    await sleep(1500);
+    await test_reconnection();
+    await sleep(1000);
     console.log("\n---------------------------- \n");
-    await test_buffer_timeout();
+    await test_segmented_message();
+    await sleep(1000);
+
+    // await test_handshake();
+    // await sleep(1000);
+    // console.log("\n---------------------------- \n");
+    // await test_buffer();
+    // await sleep(1500);
+    // console.log("\n---------------------------- \n");
+    // await test_buffer_timeout();
+
+
 }
 
 run_tests();
