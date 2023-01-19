135b2391e4812a96187315b7d59be9b103de8834
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-01-18 17:55:25 -0800

    more tests, have to fix a few things

--- tests/networking_tests.ts
+++ tests/networking_tests.ts
@@ -7,65 +7,134 @@ function sleep(ms: number) {
 const port: number = 18018;
 const ip_address: string = "127.0.0.1";
 
-// invalid connection error
+// Connecting to Node ✅, Should receive a hello message upon connecting ✅, Get peers request after hello message ✅
 async function test_connection() {
     const test_node = new LightNode();
-    test_node._client.connect(port, ip_address, () => {});
-    await sleep(1000);
+    // Should see something like this: 
 
-    if (test_node._client.connecting == false) {
-        let json_message: any = {
-            "type": "Connected to Node",
-        };
-        test_node._write(json_message);
-    }
+    // ::ffff:127.0.0.1:59377
+    // Sent: {"agent":"Marabu-Core Client 0.9","type":"hello","version":"0.9.0"}
+    // Sent: {"type":"getpeers"}
+
 }
 
-async function test_hello() {
-    const test_node = new LightNode();
-    // FILL THIS IN
-}
-
+// 4. Can disconnect then reconnect ✅
 async function test_reconnection() {
     const test_node = new LightNode();
+    await sleep(3000);
     test_node._client.destroy();
+    await sleep(3000);
     test_node._client.connect(port, ip_address, () => {});
 
-    if (test_node._client.connecting == false) {
-        let json_message: any = {
-            "type": "Connected to Node",
-        };
-        test_node._write(json_message);
-    }
+    // let json_message: any = {
+    //     "type": "hello",
+    //     "version": "0.9.0",
+    //     "agent": "Marabu Test Client"
+    // };
+    // test_node._write(json_message);
 }
 
+// 5. Get peers request return valid peers ✅
+async function test_valid_get_peers() {
+    const test_node = new LightNode();
+
+    let json_message: any = {
+        "type": "hello",
+        "version": "0.9.0",
+        "agent": "Marabu Test Client"
+    };
+    test_node._write(json_message);
+
+    let json2_message: any = {
+        "type": "getpeers",
+    };
+
+    test_node._write(json2_message);
+}
+
+// 6. Get peers segmented message return valid peers ❌
 function test_segmented_message() {
     const test_node = new LightNode();
-    test_node._client.connect(port, ip_address, () => {});
+    // test_node._client.connect(port, ip_address, () => {});
     test_node._client.write("{\"type\":\"ge\"}");
     setTimeout(() => {
         test_node._client.write(", \"tpeers\"}");
     }, 100);
-
-    test_node._client.on('data', (data: string) => {
-        const received_message = JSON.parse(data.toString());
-        if (received_message.type == "getpeers" && Array.isArray(received_message.peers)) {
-            let json_message: any = {
-                "type": "Sucess on Segmented Message"
-            };
-            test_node._write(json_message);
-        }
-    });
 }
 
-// invalid handshake error
+// 7. Get peers before hello should be invalid handshake ✅
 async function test_handshake() {
-
     const test_node = new LightNode();
     let json_message: any = {
         "type": "getpeers"
     };
     test_node._write(json_message);
+}
+
+// 8. Invalid messages invoke "INVALID_FORMAT" error ✅
+async function test_invalid_messages() {
+    const test_node = new LightNode();
+
+    let json_message: any = {
+        "type": "hello",
+        "version": "0.9.0",
+        "agent": "Marabu Test Client"
+    };
+    test_node._write(json_message);
+
+    // let json2_message: any = {
+    //     "type": "diufygeuybhv",
+    // };
+    // test_node._write(json2_message);
+
+    // let json3_message: any = {
+    //     "type": "hello",
+    // };
+    // test_node._write(json3_message);
+
+    // let json4_message: any = {
+    //     "type": "hello",
+    //     "version":"jd3.x"
+    // };
+    // test_node._write(json4_message);
+
+    // let json5_message: any = {
+    //     "type": "diufygeuybhv",
+    //     "version":"5.8.2"
+    // };
+    // test_node._write(json5_message);
+
+    // let json6_message: any = {
+    //     "Wbgygvf7rgtyv7tfbgy{{{" : "diufygeuybhv",
+    // };
+    // test_node._write(json6_message);
+
+}
+
+// 9. Sent peers that should update node's peers ❌
+async function test_update_peers() {
+    const test_node = new LightNode();
+    let json_message: any = {
+        "type": "hello",
+        "version": "0.9.0",
+        "agent": "Marabu Test Client"
+    };
+    test_node._write(json_message);
+
+    let json2_message: any = {"peers":["10.10.10.100:18018","888.888.888.8:18018","45.63.89.228:18018"],"type":"peers"};
+    test_node._write(json2_message);
+
+    await sleep(3000);
+    test_node._client.destroy();
+    await sleep(3000);
+    test_node._client.connect(port, ip_address, () => {});
+
+
+    test_node._write(json_message);
+    let json3_message: any = {
+        "type": "getpeers"
+    };
+    test_node._write(json3_message);
 }
 
 // segmented message (no timeout)
@@ -103,7 +172,7 @@ async function test_buffer_timeout() {
 
     let [peer_message_0, peer_message_1] = peer_message.split(" ");
     test_node._client.write(peer_message_0);
-    await sleep(7500);
+    await sleep(12500);
     if (!test_node._client.destroyed) {
         test_node._client.write(peer_message_1);
     }
@@ -111,23 +180,8 @@ async function test_buffer_timeout() {
 
 async function run_tests() {
 
-    await test_connection();
-    await sleep(1000);
-    console.log("\n---------------------------- \n");
-    await test_reconnection();
-    await sleep(1000);
-    console.log("\n---------------------------- \n");
-    await test_segmented_message();
-    await sleep(1000);
-
-    // await test_handshake();
-    // await sleep(1000);
-    // console.log("\n---------------------------- \n");
-    // await test_buffer();
-    // await sleep(1500);
-    // console.log("\n---------------------------- \n");
-    // await test_buffer_timeout();
-
+    await test_buffer(); // change this to test whatever test you want to run
+    await sleep(1500);
 
 }
 
